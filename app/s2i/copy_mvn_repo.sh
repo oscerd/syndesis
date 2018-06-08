58c343c3f5e47f84a2387e5176725ef440fdb699
Andrea Cosentino <ancosen@gmail.com>
2018-06-08 10:51:28 +0200

    Spike 2.21.0-SNAPSHOT gmail-connector

--- app/s2i/copy_mvn_repo.sh
+++ app/s2i/copy_mvn_repo.sh
@@ -14,4 +14,9 @@ mkdir -p "${target_dir}/m2"
 cd ${local_repo}
 find ./io/syndesis | grep -F -- "/${version}/" | grep -v -- "-sources\." | grep -v -- "-tests\." | xargs tar -c | tar -vx -C "${target_dir}/m2"
 
+camel_files=$(find ./org/apache/camel | grep -F -- "/2.21.0-SNAPSHOT/" | grep -v -- "-sources\." | grep -v -- "-tests\.")
+for file in $camel_files; do
+    cp --parents $file "${target_dir}/m2"
+done
+
 cp "${project_dir}/src/main/docker/Dockerfile" $target_dir
